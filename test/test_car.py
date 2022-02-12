ac26b50bbd1649b49f2c8d72dc217c674f4723d3
edsheeranslefttoe <91167835+edsheeranslefttoe@users.noreply.github.com>
2022-02-11 23:52:54 -0500

    Update test_car.py

--- test/test_car.py
+++ test/test_car.py
@@ -12,37 +12,21 @@ class TestCalliope(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         today = datetime.today().date()
         last_service_date = today.replace(year=today.year - 3)
-        current_mileage = 0
-        last_service_mileage = 0
-
-        car = Calliope(last_service_date, current_mileage, last_service_mileage)
-        self.assertTrue(car.needs_service())
-
-    def test_battery_should_not_be_serviced(self):
-        today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 1)
-        current_mileage = 0
-        last_service_mileage = 0
-
-        car = Calliope(last_service_date, current_mileage, last_service_mileage)
-        self.assertFalse(car.needs_service())
-
+        car = Calliope(last_service_date, 0, 0)
+        if (last_service_date >= 3):
+             self.assertTrue(car.needs_service())
+        else:
+            self.assertFalse(car.needs_service())
+       
     def test_engine_should_be_serviced(self):
         last_service_date = datetime.today().date()
         current_mileage = 30001
-        last_service_mileage = 0
 
-        car = Calliope(last_service_date, current_mileage, last_service_mileage)
-        self.assertTrue(car.needs_service())
-
-    def test_engine_should_not_be_serviced(self):
-        last_service_date = datetime.today().date()
-        current_mileage = 30000
-        last_service_mileage = 0
-
-        car = Calliope(last_service_date, current_mileage, last_service_mileage)
-        self.assertFalse(car.needs_service())
-
+        car = Calliope(last_service_date, current_mileage, 0)
+        if (self.milage > 30000):
+            self.assertTrue(car.needs_service())
+        else:
+            self.assertFalse(car.needs_service())
 
 class TestGlissade(unittest.TestCase):
     def test_battery_should_be_serviced(self):
